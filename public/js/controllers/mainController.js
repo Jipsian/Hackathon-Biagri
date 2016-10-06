99ffd32757534b838b6e07780f83821ad381a62a
CamJr <cfontainef1@gmail.com>
2016-10-06 21:05:31 +0200

    quantité fait

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -10,6 +10,43 @@ class mainController {
           accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
         });
       });
+      jQuery(document).ready(function(){
+  // This button will increment the value
+  $('#plus').click(function(e){
+      // Stop acting like a button
+      e.preventDefault();
+      // Get the field name
+      var fieldName = $(this).attr('field');
+      // Get its current value
+      var currentVal = parseInt($('input[name='+fieldName+']').val());
+      // If is not undefined
+      if (!isNaN(currentVal)) {
+          // Increment
+          $('input[name='+fieldName+']').val(currentVal + 1);
+      } else {
+          // Otherwise put a 0 there
+          $('input[name='+fieldName+']').val(0);
+      }
+  });
+  // This button will decrement the value till 0
+  $("#minus").click(function(e) {
+      // Stop acting like a button
+      e.preventDefault();
+      // Get the field name
+      var fieldName = $(this).attr('field');
+      // Get its current value
+      var currentVal = parseInt($('input[name='+fieldName+']').val());
+      // If it isn't undefined or its greater than 0
+      if (!isNaN(currentVal) && currentVal > 0) {
+          // Decrement one
+          $('input[name='+fieldName+']').val(currentVal - 1);
+      } else {
+          // Otherwise put a 0 there
+          $('input[name='+fieldName+']').val(0);
+      }
+  });
+});
+
     }
 
 }
