a177e4ff244208d98d0f86d2714e942d71a15814
CamJr <cfontainef1@gmail.com>
2016-10-06 19:05:19 +0200

    main.html 70%

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,7 +1,15 @@
 class mainController {
 
     constructor() {
-    }
+      $(document).ready(function() {
+        $('select').material_select();
+      });
 
+      $(document).ready(function(){
+        $('.collapsible').collapsible({
+          accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+        });
+      });
+    }
 
 }
